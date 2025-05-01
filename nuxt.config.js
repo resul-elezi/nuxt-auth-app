82cc1e44b61e41e132d81018b17f60f3e57a117f
Resul Elezi <resul_elezi@protonmail.com>
2025-05-01 17:42:10 +0200

    Dein Commit-Text

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,44 @@
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
   /*
    ** Global CSS
    */
-  css: [],
+  css: [
+    "primevue/resources/themes/saga-blue/theme.css",
+    "primevue/resources/primevue.min.css",
+    "primeicons/primeicons.css"
+  ],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: "~/plugins/primevue.js" }],
   /*
    ** Nuxt.js dev-modules
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
+    // Deaktivieren:
+    // '@nuxtjs/eslint-module'
   ],
   /*
    ** Nuxt.js modules
@@ -43,9 +48,10 @@ module.exports = {
    ** Build configuration
    */
   build: {
+    transpile: ["primevue"],
     /*
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
   }
-}
+};
---
+++ plugins/primevue.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+
+Vue.use(PrimeVue);
+Vue.component("Button", Button);
