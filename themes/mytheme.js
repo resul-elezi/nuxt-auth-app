0e7ed77f2ce91a8f4961fb9e21318a87d67b4d75
Resul Elezi <resul_elezi@protonmail.com>
2025-05-01 22:03:07 +0200

    #

--- themes/mytheme.js
+++ themes/mytheme.js
@@ -16,6 +16,25 @@ const MyPreset = definePreset(Aura, {
             800: '{blue.800}',
             900: '{blue.900}',
             950: '{blue.950}'
+        },
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{ocean.50}',
+                    100: '{ocean.100}',
+                    200: '{ocean.200}',
+                    300: '{ocean.300}',
+                    400: '{ocean.400}',
+                    500: '{ocean.500}',
+                    600: '{ocean.600}',
+                    700: '{ocean.700}',
+                    800: '{ocean.800}',
+                    900: '{ocean.900}',
+                    950: '{ocean.950}'
+                }
+            },
+
         }
     }
 });
