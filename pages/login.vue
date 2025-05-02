89240a8fdcd571067f5dc4b6b07de78812de4a25
Resul Elezi <resul_elezi@protonmail.com>
2025-05-02 10:19:16 +0200

    #

--- pages/login.vue
+++ pages/login.vue
@@ -16,11 +16,11 @@
             </div>
 
             <div>
-                <FloatLabel>
+                <FloatLabel variant="on">
                     <InputText id="email1" type="email" v-model="value1" class="w-full mb-4" autocomplete="off"/>
                     <label for="email1" class="text-surface-900 dark:text-surface-0 font-medium mb-2 block">Email address</label>
                 </FloatLabel>
-                <FloatLabel variant="in">
+                <FloatLabel variant="on">
                     <InputText id="password1" type="password"  v-model="value2" class="w-full mb-4"/>
                     <label for="password1" class="text-surface-900 dark:text-surface-0 font-medium mb-2 block">Password</label>
                 </FloatLabel>
