707341573c86ef4115f2c157934a791bd425ba8b
Resul Elezi <resul_elezi@protonmail.com>
2025-05-02 09:01:17 +0200

    #

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/eslint',
     '@primevue/nuxt-module',
-    '@nuxtjs/tailwindcss
+    '@nuxtjs/tailwindcss'
     ],
     primevue: {
       importTheme: { from: '@/themes/mytheme.js' },
