546494490de71dc248461ebf15f1290734256a0b
Resul Elezi <resul_elezi@protonmail.com>
2025-05-02 08:03:36 +0200

    #

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>Index</div>
+  <div>
+    <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit" class="flex flex-col gap-4 w-full sm:w-56">
+      <div class="flex flex-col gap-1">
+          <InputText name="email" type="text" placeholder="Email" />
+          <Message v-if="$form.username?.invalid" severity="error" size="small" variant="simple">{{ $form.username.error?.message }}</Message>
+      </div>
+      <Button type="submit" severity="secondary" label="Submit" />
+  </Form>
+  </div>
 </template>
 
 <script setup>
+import { Form } from '@primevue/forms';
+import Button from 'primevue/button';
+
 // export default {
 
 // }
